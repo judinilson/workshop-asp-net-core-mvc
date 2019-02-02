6c18b90404ff3235709c757ad3a3234b95cdb6d4
judinilson <judinilson0000@gmail.com>
2019-02-02 17:16:35 -0200

    Crud scaffolding

---
+++ salesWebMvc/Data/salesWebMvcContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace salesWebMvc.Models
+{
+    public class salesWebMvcContext : DbContext
+    {
+        public salesWebMvcContext (DbContextOptions<salesWebMvcContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<salesWebMvc.Models.Department> Department { get; set; }
+    }
+}
